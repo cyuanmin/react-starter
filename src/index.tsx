d5323ed14a39adfdfbf5e085b300306b7c0052f4
Chen <Yuanming.Chen2@Fiserv.com>
2017-10-20 11:26:19 -0600

    Added basic code that run react!

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,9 @@
-console.log("Hello world");
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Hello } from "./components/Hello";
+
+ReactDOM.render(
+    <Hello compiler="TypeScript" framework="React" />,
+    document.getElementById("example")
+);
